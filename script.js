3d48b79ec8bac0eacd106ade67855a1ac9e86435
MohamedDanis <danishmanu17@gmail.com>
2023-05-21 17:01:20 +0530

    started responsive

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.getElementsByClassName('footerfeat')
+// code for hamburger menu, when clicked new menu opens and closes
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+const menu = document.querySelector(".nav-list");
+
+//code for hamburger lines to change to X when clicked and back to lines when clicked again 
+hamburger.addEventListener("click", () => {
+  document.getElementsByClassName("line")[0].classList.toggle("line1");
+    document.getElementsByClassName("line")[1].classList.toggle("line2");
+    document.getElementsByClassName("line")[2].classList.toggle("line3");
+    menu.classList.toggle("open");
+    
+}
+);
+
+
